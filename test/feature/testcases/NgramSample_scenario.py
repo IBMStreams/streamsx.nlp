11276d463cc5abaa126c1c4da606a18e89de347b
Mark-Oliver Heger <mark-oliver.heger@de.ibm.com>
2016-11-15 12:34:14 +0100

    test case file added for NgramSample, toolkit version 1.1.0

---
+++ test/feature/testcases/NgramSample_scenario.py
@@ -0,0 +1,17 @@
+# Copyright (C)2016, International Business Machines Corporation
+# All rights reserved.
+
+import testutils as tt
+
+def test_execution(name, python_command):
+	'''Execute the test case'''
+	tt.run_checked(['output/bin/standalone'])
+	tt.run_checked(['diff', 'data/out.txt', 'data/expected.txt'])
+	tt.run_checked(['output/NgramFuncSample/bin/standalone'])
+	tt.run_checked(['diff', 'data/out2.txt', 'data/expected.txt'])
+
+def test_cleanup(name):
+	'''Removes all output files which are produced during test execution'''
+	tt.remove_f('data/out.txt')
+	tt.remove_f('data/out2.txt')
+
