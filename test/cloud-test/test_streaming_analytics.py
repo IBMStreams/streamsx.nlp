c286d2f1e06f1b104b70ea19a8a5beb830bbe1cb
streamsadmin <streamsadmin@localhost.localdomain>
2018-02-13 13:52:01 +0100

    update cloud test - optimize toolkits to be uploaded

--- test/cloud-test/test_streaming_analytics.py
+++ test/cloud-test/test_streaming_analytics.py
@@ -17,25 +17,13 @@ class TestCloud(unittest.TestCase):
     def setUp(self):
         Tester.setup_streaming_analytics(self, force_remote_build=False)
 
-    def _add_toolkits(self, topo):
-        tk.add_toolkit(topo, '../../samples/ContentRankingSample')
-        tk.add_toolkit(topo, '../../samples/DictionaryFilterSample')
-        tk.add_toolkit(topo, '../../samples/FunctionsSample')
-        tk.add_toolkit(topo, '../../samples/LemmatizerSample')
-        tk.add_toolkit(topo, '../../samples/LinearClassificationSample')
-        tk.add_toolkit(topo, '../../samples/LinearClassificationSplPy')
-        tk.add_toolkit(topo, '../../samples/NgramBasicSample')
-        tk.add_toolkit(topo, '../../samples/NgramSample')
-        tk.add_toolkit(topo, '../../samples/RutaTextSample')
-        tk.add_toolkit(topo, '../../samples/TfIdfSample')
-        tk.add_toolkit(topo, '../../samples/UimaTextGeneratedTypes')
-        tk.add_toolkit(topo, '../../samples/UimaTextSample')
-
+    def _add_toolkits(self, topo, toolkit_name):
+        tk.add_toolkit(topo, toolkit_name)
         tk.add_toolkit(topo, '../../com.ibm.streamsx.nlp')
 
-    def _build_launch_validate(self, name, composite_name, parameters):
+    def _build_launch_validate(self, name, composite_name, parameters, toolkit_name):
         topo = Topology(name)
-        self._add_toolkits(topo)
+        self._add_toolkits(topo, toolkit_name)
 	
         params = parameters
         # Call the test composite
@@ -49,40 +37,40 @@ class TestCloud(unittest.TestCase):
 
 # missing python modules
 #    def test_content_ranking(self):
-#        self._build_launch_validate("test_content_ranking", "nlp.sample::ContentRankingComp", {'pythonCommand':'$PYTHONHOME/bin/python3'})
+#        self._build_launch_validate("test_content_ranking", "nlp.sample::ContentRankingComp", {'pythonCommand':'$PYTHONHOME/bin/python3'}, '../../samples/ContentRankingSample')
 
     def test_dictionary_filter(self):
-        self._build_launch_validate("test_dictionary_filter", "nlp.sample::DictionaryFilterComp", {})
+        self._build_launch_validate("test_dictionary_filter", "nlp.sample::DictionaryFilterComp", {}, '../../samples/DictionaryFilterSample')
 
     def test_functions(self):
-        self._build_launch_validate("test_functions", "nlp.sample::FunctionsComp", {})
+        self._build_launch_validate("test_functions", "nlp.sample::FunctionsComp", {}, '../../samples/FunctionsSample')
 
     def test_lemmatizer(self):
-        self._build_launch_validate("test_lemmatizer", "nlp.sample::LemmatizerComp", {})
+        self._build_launch_validate("test_lemmatizer", "nlp.sample::LemmatizerComp", {}, '../../samples/LemmatizerSample')
 
     def test_linear_classification(self):
-        self._build_launch_validate("test_linear_classification", "nlp.sample::LinearClassificationComp", {'pythonCommand':'$PYTHONHOME/bin/python3'})
+        self._build_launch_validate("test_linear_classification", "nlp.sample::LinearClassificationComp", {'pythonCommand':'$PYTHONHOME/bin/python3'},'../../samples/LinearClassificationSample')
 
     def test_linear_classification_py(self):
-        self._build_launch_validate("test_linear_classification_py", "nlp.sample::LinearClassificationPyComp", {})
+        self._build_launch_validate("test_linear_classification_py", "nlp.sample::LinearClassificationPyComp", {}, '../../samples/LinearClassificationSplPy')
 
     def test_ngram_basic(self):
-        self._build_launch_validate("test_ngram_basic", "nlp.sample::NgramBasicComp", {})
+        self._build_launch_validate("test_ngram_basic", "nlp.sample::NgramBasicComp", {},'../../samples/NgramBasicSample')
 
     def test_ngram(self):
-        self._build_launch_validate("test_ngram", "nlp.sample::NgramComp", {})
+        self._build_launch_validate("test_ngram", "nlp.sample::NgramComp", {}, '../../samples/NgramSample')
 
     def test_ruta_text(self):
-        self._build_launch_validate("test_ruta_text", "nlp.sample::RutaTextComp", {'loop':1})
+        self._build_launch_validate("test_ruta_text", "nlp.sample::RutaTextComp", {'loop':1}, '../../samples/RutaTextSample')
 
     def test_tfidf(self):
-        self._build_launch_validate("test_tfidf", "nlp.sample::TfIdfComp", {})
+        self._build_launch_validate("test_tfidf", "nlp.sample::TfIdfComp", {}, '../../samples/TfIdfSample')
 
     def test_uima_text_gen_types(self):
-        self._build_launch_validate("test_uima_text_gen_types", "nlp.sample::UimaTextGeneratedTypesComp", {})
+        self._build_launch_validate("test_uima_text_gen_types", "nlp.sample::UimaTextGeneratedTypesComp", {}, '../../samples/UimaTextGeneratedTypes')
 
     def test_uima_text(self):
-        self._build_launch_validate("test_uima_text", "nlp.sample::UimaTextComp", {})
+        self._build_launch_validate("test_uima_text", "nlp.sample::UimaTextComp", {}, '../../samples/UimaTextSample')
 
 
 
