d8ae68049dc648fa8df5b586d96319e4b9ffaebc
markheger <mark-oliver.heger@de.ibm.com>
2018-10-19 15:36:27 +0200

    resolve #72

--- test/cloud-test/test_streaming_analytics.py
+++ test/cloud-test/test_streaming_analytics.py
@@ -15,7 +15,7 @@ class TestCloud(unittest.TestCase):
         self.service_name = os.environ.get('STREAMING_ANALYTICS_SERVICE_NAME')
 
     def setUp(self):
-        Tester.setup_streaming_analytics(self, force_remote_build=False)
+        Tester.setup_streaming_analytics(self, force_remote_build=True)
 
     def _add_toolkits(self, topo, toolkit_name):
         tk.add_toolkit(topo, toolkit_name)
@@ -24,6 +24,9 @@ class TestCloud(unittest.TestCase):
     def _build_launch_validate(self, name, composite_name, parameters, toolkit_name):
         topo = Topology(name)
         self._add_toolkits(topo, toolkit_name)
+        if name == 'test_content_ranking':
+            topo.add_pip_package('Theano==0.8.2')
+            topo.add_pip_package('Keras==1.0.7')
 	
         params = parameters
         # Call the test composite
@@ -35,9 +38,8 @@ class TestCloud(unittest.TestCase):
 
         tester.test(self.test_ctxtype, self.test_config)
 
-# missing python modules
-#    def test_content_ranking(self):
-#        self._build_launch_validate("test_content_ranking", "nlp.sample::ContentRankingComp", {'pythonCommand':'$PYTHONHOME/bin/python3'}, '../../samples/ContentRankingSample')
+    def test_content_ranking(self):
+        self._build_launch_validate("test_content_ranking", "nlp.sample::ContentRankingComp", {'pythonCommand':'$PYTHONHOME/bin/python3'}, '../../samples/ContentRankingSample')
 
     def test_dictionary_filter(self):
         self._build_launch_validate("test_dictionary_filter", "nlp.sample::DictionaryFilterComp", {}, '../../samples/DictionaryFilterSample')
