5c464d5191a36a183e4401437bd2dc700a9ae5d0
markheger <mark-oliver.heger@de.ibm.com>
2018-11-08 12:44:05 +0100

    test scripts updated for icp

--- test/cloud-test/test_nlp.py
+++ test/cloud-test/test_nlp.py
@@ -22,11 +22,12 @@ class TestCloud(unittest.TestCase):
         tk.add_toolkit(topo, '../../com.ibm.streamsx.nlp')
 
     def _build_launch_validate(self, name, composite_name, parameters, toolkit_name):
+        print ("------ "+name+" ------")        
         topo = Topology(name)
         self._add_toolkits(topo, toolkit_name)
-        if name == 'test_content_ranking':
-            topo.add_pip_package('Theano==0.8.2')
-            topo.add_pip_package('Keras==1.0.7')
+        #if name == 'test_content_ranking':
+        #    topo.add_pip_package('Theano==0.8.2')
+        #    topo.add_pip_package('Keras==1.0.7')
 	
         params = parameters
         # Call the test composite
@@ -36,10 +37,18 @@ class TestCloud(unittest.TestCase):
         tester.tuple_count(test_op.stream, 1, exact=True)
         tester.contents(test_op.stream, [{'result':'ok'}] )
 
-        tester.test(self.test_ctxtype, self.test_config)
+        cfg = {}
+        job_config = streamsx.topology.context.JobConfig(tracing='warn')
+        # icp config
+        if ("TestICP" in str(self)):
+            job_config.raw_overlay = {"configInstructions": {"convertTagSet": [ {"targetTagSet":["python"] } ]}}
+        job_config.add(cfg)
 
-    def test_content_ranking(self):
-        self._build_launch_validate("test_content_ranking", "nlp.sample::ContentRankingComp", {'pythonCommand':'$PYTHONHOME/bin/python3'}, '../../samples/ContentRankingSample')
+        tester.test(self.test_ctxtype, cfg)
+        print (str(tester.result))
+
+#    def test_content_ranking(self):
+#        self._build_launch_validate("test_content_ranking", "nlp.sample::ContentRankingComp", {'pythonCommand':'$PYTHONHOME/bin/python3'}, '../../samples/ContentRankingSample')
 
     def test_dictionary_filter(self):
         self._build_launch_validate("test_dictionary_filter", "nlp.sample::DictionaryFilterComp", {}, '../../samples/DictionaryFilterSample')
@@ -75,4 +84,15 @@ class TestCloud(unittest.TestCase):
         self._build_launch_validate("test_uima_text", "nlp.sample::UimaTextComp", {}, '../../samples/UimaTextSample')
 
 
+class TestICP(TestCloud):
+    """ Test invocations of composite operators in remote Streams instance using local toolkit """
+
+    @classmethod
+    def setUpClass(self):
+        print (str(self))
+
+    def setUp(self):
+        Tester.setup_distributed(self)
+
+        
 
